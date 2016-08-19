b2cdcbe24a33188cdf3e36c140c17b8e6297ce0c
Bob Saveland <bob.saveland@teamaol.com>
2016-08-19 18:04:58 -0400

    put partition offset log into one event instead of one per partition

--- src/main/ruby/i/hate/java/offset_skew.rb
+++ src/main/ruby/i/hate/java/offset_skew.rb
@@ -11,9 +11,6 @@ class OffsetSkew
     mins_early = @config["lookbackMinutes"] || 60
     @earliest = (Time.now.to_i - (mins_early.to_i * 60)) * 1000
     @latest = -1
-    @logger.log "Starting with zookeeper: #{@config['zookeeperUrl']}"
-    @logger.log "Starting with lookback: #{mins_early} minutes"
-    @logger.log "Starting with topics: #{@config['topics'].inspect}"
     ZookeeperUtils.connect @config['zookeeperUrl']
     topics = @config['topics']
     topics.each {|topic| partition_skew topic}
@@ -23,15 +20,17 @@ class OffsetSkew
   def partition_skew(topic)
     brokers = ZookeeperUtils.brokers
     partitions = ZookeeperUtils.partitions topic
-    results = []
+    results = {}
     partitions.each do |partition|
       broker = brokers.find {|b| b[:id].to_i == partition[:leader]}
       @logger.log "No broker found for partition: #{partition[:partition]}" unless broker
       delta = offset_delta(broker, partition[:partition].to_i, topic) if broker
-      results << delta
+      results[partition[:partition]] = delta
     end
-    min = results.compact.min
-    max = results.compact.max
+    @logger.log results.map {|k,v| "Topic: #{topic} partition: #{k} has delta of: #{v}"}.join("\n")
+    deltas = results.map {|k,v| v}.compact
+    min = deltas.min
+    max = deltas.max
     skew = (max.to_f - min.to_f) / min.to_f
     skew = 0.0 if skew.send("nan?") #stupid java and ? methods
     skew = 100000.0 if skew.send("infinite?")
@@ -51,7 +50,6 @@ class OffsetSkew
       earliest_offset = early_resp.partition_error_and_offsets[t_and_p].offsets.last
       latest_offset = late_resp.partition_error_and_offsets[t_and_p].offsets.last
       delta = latest_offset - earliest_offset
-      @logger.log "Topic: #{topic} partition: #{partition} has delta of: #{delta}\n"
       return delta
     rescue
       @logger.log "Unable to find offset for partition: #{partition} on broker: #{broker}\n"
